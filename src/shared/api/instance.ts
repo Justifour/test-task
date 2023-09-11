d91623c05a93aed54bbefb898372b52928c64f97
Justifour <58982535+Justifour@users.noreply.github.com>
2023-09-11 15:33:06 +0300

    updated access token

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: `https://api.github.com/search/`,
   timeout: 1000,
   headers: {
-    Authorization: "Bearer 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"
+    Authorization: "Bearer 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"
   }
 });
